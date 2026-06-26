5cb8ab72847e2095e805b8750fc4021607b6e498
kingharoldrelox-spec <kingharoldrelox@gmail.com>
2026-06-27 00:01:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
 const http = require('http');
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates // Kailangan ito para sa voice channel
     ]
 });
 
-// Ito ang taga-gising sa bot para hindi matulog sa Render
+// Taga-gising sa Render
 http.createServer((req, res) => {
     res.write("Buhay ang bot mo!");
     res.end();
@@ -17,12 +19,17 @@ http.createServer((req, res) => {
 
 client.once('ready', () => {
     console.log(`Yown! Online na si ${client.user.tag}!`);
-});
-
-// Kapag may nag-chat ng "ping", sasagot ang bot ng "pong"
-client.on('messageCreate', message => {
-    if (message.content.toLowerCase() === 'ping') {
-        message.reply('pong! 🏓');
+    
+    // DITO PAPASOK ANG BOT SA VOICE CHANNEL:
+    try {
+        joinVoiceChannel({
+            channelId: '1520037306579550421', // I-paste dito ang Voice Channel ID
+            guildId: '1520037306042417243',    // I-paste dito ang Server ID
+            adapterCreator: client.guilds.cache.get('PALITAN_MO_ITO_NG_SERVER_ID').voiceAdapterCreator, // I-paste din dito ang Server ID
+        });
+        console.log("Nakasok na sa Voice Channel ang Bot!");
+    } catch (error) {
+        console.error("May mali sa pagpasok sa voice:", error);
     }
 });
 
